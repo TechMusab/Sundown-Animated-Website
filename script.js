4940cb3611628a817a905d535faec620dbbb8b16
Musab Bin Ubaid <joiya4636@gmail.com>
2024-07-06 15:10:25 -0700

    tabs functionality

--- script.js
+++ script.js
@@ -2,25 +2,46 @@
 //     el: document.querySelector('.main'),
 //     smooth: true
 // });
-const container=document.querySelector('.elementcontainer');
-const imagediv=document.querySelector('.imagediv');
-container.addEventListener('mouseenter', function(){
-imagediv.style.display="block";
+const container = document.querySelector(".elementcontainer");
+const imagediv = document.querySelector(".imagediv");
+container.addEventListener("mouseenter", function () {
+  imagediv.style.display = "block";
 });
-container.addEventListener('mouseleave', function(){
-imagediv.style.display="none";
+container.addEventListener("mouseleave", function () {
+  imagediv.style.display = "none";
 });
-const images=document.querySelectorAll('.element');
-images.forEach(function(image){
-    image.addEventListener('mouseenter',function(){
-        let img=image.getAttribute("data-image");
-        console.log(img);
-        imagediv.style.backgroundImage=`url(${img})`;
-    })
-})
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: "auto",
-    centeredSlides: true,
-    spaceBetween: 100,
-   
+const images = document.querySelectorAll(".element");
+images.forEach(function (image) {
+  image.addEventListener("mouseenter", function () {
+    let img = image.getAttribute("data-image");
+    console.log(img);
+    imagediv.style.backgroundImage = `url(${img})`;
   });
+});
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  centeredSlides: true,
+  spaceBetween: 100,
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const headings = document.querySelectorAll(".tabsheading h1");
+  const containers = {
+    Design: document.getElementById("design-container"),
+    Project: document.getElementById("project-container"),
+    Execution: document.getElementById("execution-container"),
+  };
+
+  headings.forEach((heading) => {
+    heading.addEventListener("click", function () {
+      const tabName = heading.innerText;
+      Object.values(containers).forEach(
+        (container) => (container.style.display = "none")
+      );
+      containers[tabName].style.display = "flex";
+    });
+  });
+  containers["Design"].style.display = "flex";
+  containers["Project"].style.display = "none";
+  containers["Execution"].style.display = "none";
+});
