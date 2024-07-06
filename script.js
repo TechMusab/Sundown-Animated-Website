82b1c58a87d5dcb9dcacd87c50b7bb3daf6f260c
Musab Bin Ubaid <joiya4636@gmail.com>
2024-07-05 18:24:09 -0700

    added some javascript

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('.main'),
     smooth: true
 });
+const container=document.querySelector('.elementcontainer');
+const imagediv=document.querySelector('.imagediv');
+container.addEventListener('mouseenter', function(){
+imagediv.style.display="block";
+});
+container.addEventListener('mouseleave', function(){
+imagediv.style.display="none";
+});
+const images=document.querySelectorAll('.element');
+images.forEach(function(image){
+    image.addEventListener('mouseenter',function(){
+        let img=image.getAttribute("data-image");
+        console.log(img);
+        imagediv.style.backgroundImage=`url(${img})`;
+    })
+})
